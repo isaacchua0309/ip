202a4c53629767717b83c26b0f7d0ef40572f24e
Isaac Chua <isaacchuaqx@gmail.com>
2025-01-24 13:06:43 +0800

    Add more personality and engagement to Boblet

--- src/main/java/Boblet.java
+++ src/main/java/Boblet.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Boblet {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Boblet");
-        System.out.println("What can I do for you?");
+        System.out.println("Hey there! I'm Boblet, your friendly assistant!");
+        System.out.println("I can keep track of your tasks. Just tell me what to do!");
+        System.out.println("Type 'list' to see your tasks or 'bye' to leave. Let's get started!");
         System.out.println("____________________________________________________________");
 
         String[] tasks = new String[100];
@@ -12,21 +13,28 @@ public class Boblet {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.print("You: "); // Prompt to show user input
             String userInput = scanner.nextLine();
 
             // Exit the program if user types "bye"
-            if (userInput.equals("bye")) {
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Aww, you're leaving already? Well, take care!");
+                System.out.println("See you soon! Bye from Boblet!");
                 System.out.println("____________________________________________________________");
                 break;
             }
 
             // Display all tasks if user types "list"
-            if (userInput.equals("list")) {
+            if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                if (taskCount == 0) {
+                    System.out.println("Your task list is empty! Add something to get started.");
+                } else {
+                    System.out.println("Here's what you've got so far:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
                 }
                 System.out.println("____________________________________________________________");
             } else {
@@ -35,7 +43,9 @@ public class Boblet {
                 taskCount++;
 
                 System.out.println("____________________________________________________________");
-                System.out.println("added: " + userInput);
+                System.out.println("Got it! I've added this to your list:");
+                System.out.println("  " + userInput);
+                System.out.println("You now have " + taskCount + " task(s) in your list. Keep it up!");
                 System.out.println("____________________________________________________________");
             }
         }
