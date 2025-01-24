001b7c432436153d3b989b636828078b8dccce4f
Isaac Chua <isaacchuaqx@gmail.com>
2025-01-24 12:50:17 +0800

    Add Level-1 functionality: Greet, Echo, Exit

--- src/main/java/Boblet.java
+++ src/main/java/Boblet.java
@@ -1,19 +1,35 @@
+import java.util.Scanner;
+
 public class Boblet {
     public static void main(String[] args) {
-        try {
-            System.out.println("____________________________________________________________\n");
-            Thread.sleep(1000);
-            System.out.println("Hello! I'm Boblet\n");
-            Thread.sleep(1000);
-            System.out.println("What can I do for you?\n");
-            System.out.println("\n");
-            Thread.sleep(1000);
-            System.out.println("____________________________________________________________\n");
-            Thread.sleep(1000);
-            System.out.println("Bye. Hope to see you again soon!\n");
-            System.out.println("____________________________________________________________\n");
-        } catch (InterruptedException e) {
-            System.err.println("Thread was interrupted!");
+        // Print greeting
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Boblet");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Create a scanner to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Keep reading commands until "bye" is entered
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            // Exit if the user types "bye"
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
         }
+
+        // Close the scanner
+        scanner.close();
     }
 }
