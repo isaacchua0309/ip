3ffec6eb20910cfc66efc561226a4e6f249d1c08
Isaac Chua <isaacchuaqx@gmail.com>
2025-01-24 12:57:03 +0800

    Add Level-2 functionality: Add and List

--- src/main/java/Boblet.java
+++ src/main/java/Boblet.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class Boblet {
     public static void main(String[] args) {
-        // Print greeting
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Boblet");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Create a scanner to read input
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
 
-        // Keep reading commands until "bye" is entered
         while (true) {
             String userInput = scanner.nextLine();
 
-            // Exit if the user types "bye"
+            // Exit the program if user types "bye"
             if (userInput.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
@@ -23,13 +22,23 @@ public class Boblet {
                 break;
             }
 
-            // Echo the user's input
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
-        }
+            // Display all tasks if user types "list"
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                // Add the task to the array
+                tasks[taskCount] = userInput;
+                taskCount++;
 
-        // Close the scanner
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
         scanner.close();
     }
 }
